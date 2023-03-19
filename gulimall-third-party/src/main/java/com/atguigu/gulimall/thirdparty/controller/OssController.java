1c2cc36e91ddbac9c274631e4342945135673ad7
Leo <2422737092@qq.com>
2023-03-19 22:22:05 +0800

    细节完善

--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
@@ -1,12 +1,10 @@
 package com.atguigu.gulimall.thirdparty.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
+import com.atguigu.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,7 +37,7 @@ public class OssController {
     String accessId;
 
     @RequestMapping("/oss/policy")
-    public  Map <String, String> policy(){
+    public R policy(){
         // 填写Host地址，格式为https://bucketname.endpoint。
         String host = "https://"+bucket+"."+endpoint;
         // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
@@ -75,6 +73,6 @@ public class OssController {
             // Assert.fail(e.getMessage());
             System.out.println(e.getMessage());
         }
-        return respMap;
+        return R.ok().put("data",respMap);
     }
 }
