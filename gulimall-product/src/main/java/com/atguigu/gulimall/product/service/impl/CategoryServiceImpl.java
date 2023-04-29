ae94d9dcdbf3eb46d269e64fff856d1f17b230ce
Leo <2422737092@qq.com>
2023-04-29 16:29:33 +0800

    缓存-@Cacheable细节设置

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -103,8 +103,43 @@ public class CategoryServiceImpl extends ServiceImpl <CategoryDao, CategoryEntit
         }
     }
 
-    // 每一个需要缓存的数据我们都来指定放到那个名字的缓存。【缓存的分区（按照业务类型划分）】
-    @Cacheable({"category"})//代表当前方法的结果需要缓存。如果缓存中有，方法不用调用。如果缓存中没有会调用方法
+    /**
+     * 1、每一个需要缓存的数据我们都来指定要放到那个名字的缓存。【缓存的分区(按照业务类型分)】
+     * 2、 @Cacheable({"category"})
+     *      代表当前方法的结果需要缓存，如果缓存中有，方法不用调用。
+     *      如果缓存中没有，会调用方法，最后将方法的结果放入缓存
+     * 3、默认行为
+     *      1）、如果缓存中有，方法不用调用。
+     *      2）、key默认自动生成；缓存的名字::SimpleKey [](自主生成的key值)
+     *      3）、缓存的value的值。默认使用jdk序列化机制，将序列化后的数据存到redis
+     *      4）、默认ttl时间 -1；
+     *
+     *    自定义：
+     *      1）、指定生成的缓存使用的key：  key属性指定，接受一个SpEL
+     *             SpEL的详细https://docs.spring.io/spring/docs/5.1.12.RELEASE/spring-framework-reference/integration.html#cache-spel-context
+     *      2）、指定缓存的数据的存活时间： 配置文件中修改ttl
+     *      3）、将数据保存为json格式:
+     *              自定义RedisCacheConfiguration即可
+     * 4、Spring-Cache的不足；
+     *      1）、读模式：
+     *          缓存穿透：查询一个null数据。解决：缓存空数据；ache-null-values=true
+     *          缓存击穿：大量并发进来同时查询一个正好过期的数据。解决：加锁；？默认是无加锁的;sync = true（加锁，解决击穿）
+     *          缓存雪崩：大量的key同时过期。解决：加随机时间。加上过期时间。：spring.cache.redis.time-to-live=3600000
+     *      2）、写模式：（缓存与数据库一致）
+     *          1）、读写加锁。
+     *          2）、引入Canal，感知到MySQL的更新去更新数据库
+     *          3）、读多写多，直接去数据库查询就行
+     *    总结：
+     *      常规数据（读多写少，即时性，一致性要求不高的数据）；完全可以使用Spring-Cache；写模式（只要缓存的数据有过期时间就足够了）
+     *      特殊数据：特殊设计
+     *
+     *   原理：
+     *      CacheManager(RedisCacheManager)->Cache(RedisCache)->Cache负责缓存的读写
+     *
+     *
+     * @return
+     */
+    @Cacheable(value = {"category"},key = "#root.method.name")
     @Override
     public List <CategoryEntity> getLevelOneCategorys() {
         long l = System.currentTimeMillis();
@@ -191,7 +226,16 @@ public class CategoryServiceImpl extends ServiceImpl <CategoryDao, CategoryEntit
         return catelogMap;
     }
 
+    /**
+     * 缓存里面的数据如何和数据库保持一致
+     * 缓存数据一致性
+     * 1）、双写模式
+     * 2）、失效模式
+     * @return
+     */
     public Map<String, List<Catelog2Vo>> getCatalogJsonFromDbWithRedission() {
+        // 锁的名字。锁的粒度，越细越快
+        // 锁的粒度：具体缓存的是某个数据，n-号商品：product-11-lock product-12-lock 和product-lock相比，前者的粒度更小，速度会更快~
         RLock lock = redisson.getLock("CatalogJson-lock");
         lock.lock();
         Map<String, List<Catelog2Vo>> dataFromDb;
@@ -230,6 +274,7 @@ public class CategoryServiceImpl extends ServiceImpl <CategoryDao, CategoryEntit
 
             //获取值对比+对比成功删除=原子操作  lua脚本解锁
 //            String lockValue = redisTemplate.opsForValue().get("lock");
+//            我自己的锁UUID肯定知道哇
 //            if(uuid.equals(lockValue)){
 //                //删除我自己的锁
 //                redisTemplate.delete("lock");//删除锁
@@ -246,6 +291,31 @@ public class CategoryServiceImpl extends ServiceImpl <CategoryDao, CategoryEntit
             }
             return getCatalogJsonFromDbWithRedisLock();//自旋的方式
         }
+    }
+
+    /**
+     * 从数据库查询并封装分类数据【本地锁】
+     * @return
+     */
+    public Map<String, List<Catelog2Vo>> getCatalogJsonFromDbWithLocalLock() {
+//        //1、如果缓存中有就用缓存的
+//        Map<String, List<Catelog2Vo>> catalogJson = (Map<String, List<Catelog2Vo>>) cache.get("catalogJson");
+//        if(cache.get("catalogJson") == null) {
+//            //调用业务  xxxxx
+//            //返回数据又放入缓存
+//            cache.put("catalogJson",parent_cid);
+//        }
+//        return catalogJson;
+        //只要是同一把锁，就能锁住需要这个锁的所有线程
+        //1、synchronized (this)：SpringBoot所有的组件在容器中都是单例的。
+        //TODO 本地锁：synchronized，JUC（Lock），在分布式情况下，想要锁住所有，必须使用分布式锁
+
+        synchronized (this) {
+            //得到锁以后，我们应该再去缓存中确定一次，如果没有才需要继续查询
+            return getCatelogJsonFromDB();
+        }
+
+
     }
 
     /**
