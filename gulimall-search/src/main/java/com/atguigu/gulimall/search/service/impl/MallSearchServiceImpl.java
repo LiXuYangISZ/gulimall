680a899caa16b1def9b52c92f1cd625cd9ee6d2f
Leo <2422737092@qq.com>
2023-05-01 12:39:04 +0800

    检索服务-SearchRequest构建-聚合

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/service/impl/MallSearchServiceImpl.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/service/impl/MallSearchServiceImpl.java
@@ -15,6 +15,10 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.NestedQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.nested.NestedAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.elasticsearch.search.sort.SortOrder;
@@ -61,8 +65,9 @@ public class MallSearchServiceImpl implements MallSearchService {
     /**
      * 准备检索请求
      * 模糊匹配 ，过滤（按照属性、分类、品牌、价格区间、库存），排序，分页，高亮，聚合分析
-     * @return
+     *
      * @param param
+     * @return
      */
     private SearchRequest buildSearchRequest(SearchParam param) {
         // 1.构建DSL语句
@@ -73,21 +78,21 @@ public class MallSearchServiceImpl implements MallSearchService {
          * 模糊匹配 ，过滤（按照属性、分类、品牌、价格区间、库存）
          */
         // 2.1 must-模糊匹配
-        if(StringUtils.isNotBlank(param.getKeyword())){
-            boolQueryBuilder.must(QueryBuilders.matchQuery("skuTitle",param.getKeyword()));
+        if (StringUtils.isNotBlank(param.getKeyword())) {
+            boolQueryBuilder.must(QueryBuilders.matchQuery("skuTitle", param.getKeyword()));
         }
         // 2.2 filter
         // 2.2.1 按照三级分类id查询
-        if(param.getCatalog3Id()!=null){
-            boolQueryBuilder.filter(QueryBuilders.termQuery("catalogId",param.getCatalog3Id()));
+        if (param.getCatalog3Id() != null) {
+            boolQueryBuilder.filter(QueryBuilders.termQuery("catalogId", param.getCatalog3Id()));
         }
         // 2.2.2 按照品牌id进行查询
-        if(param.getBrandId()!=null && param.getBrandId().size() > 0){
-            boolQueryBuilder.filter(QueryBuilders.termsQuery("brandId",param.getBrandId()));
+        if (param.getBrandId() != null && param.getBrandId().size() > 0) {
+            boolQueryBuilder.filter(QueryBuilders.termsQuery("brandId", param.getBrandId()));
         }
 
         // 2.2.3 按照属性查询
-        if(param.getAttrs()!=null && param.getAttrs().size() > 0){
+        if (param.getAttrs() != null && param.getAttrs().size() > 0) {
             // 注意：为啥93、103-104要写在For里面呢，因为如果写在外边就意味着，所有的attr都要参与满足，这显然是不合理的。
             // 举例：attrs=1_麒麟9000:A16&attrs_2_16g:32g;  CPU品牌要同时满足麒麟9000或A16。且内存还要同时满足16g或32g
             // 解决方案：为每一个属性都构建一个filter，这样所有满足的都会包括进来
@@ -99,9 +104,8 @@ public class MallSearchServiceImpl implements MallSearchService {
                 String attrId = str[0];
                 // 属性值
                 String[] attrValues = str[1].split(":");
-                // TODO 这里可以尝试下链式写法
-                nestedBoolQueryBuilder.must(QueryBuilders.termQuery("attrs.attrId",attrId))
-                        .must(QueryBuilders.termsQuery("attrs.attrValue",attrValues));
+                nestedBoolQueryBuilder.must(QueryBuilders.termQuery("attrs.attrId", attrId))
+                        .must(QueryBuilders.termsQuery("attrs.attrValue", attrValues));
                 NestedQueryBuilder nestedQueryBuilder = QueryBuilders.nestedQuery("attrs", nestedBoolQueryBuilder, ScoreMode.None);
                 boolQueryBuilder.filter(nestedQueryBuilder);
             }
@@ -109,20 +113,20 @@ public class MallSearchServiceImpl implements MallSearchService {
         }
 
         // 2.2.4 按照是否有库存查询【不传就是查询全部】
-        if(param.getHasStock() != null){
-            boolQueryBuilder.filter(QueryBuilders.termQuery("hasStock",param.getHasStock()==1));
+        if (param.getHasStock() != null) {
+            boolQueryBuilder.filter(QueryBuilders.termQuery("hasStock", param.getHasStock() == 1));
         }
 
         // 2.2.5 按照价格区间进行查询
-        if(StringUtils.isNotBlank(param.getSkuPrice())){
+        if (StringUtils.isNotBlank(param.getSkuPrice())) {
             RangeQueryBuilder rangeQueryBuilder = QueryBuilders.rangeQuery("skuPrice");
             // 注意 _500永远是2,500_永远是1
             String[] str = param.getSkuPrice().split("_");
-            if(param.getSkuPrice().startsWith("_")){
+            if (param.getSkuPrice().startsWith("_")) {
                 rangeQueryBuilder.lte(str[1]);
-            }else if(param.getSkuPrice().endsWith("_")){
+            } else if (param.getSkuPrice().endsWith("_")) {
                 rangeQueryBuilder.gte(str[0]);
-            }else{
+            } else {
                 rangeQueryBuilder.gte(str[0]).lte(str[1]);
             }
             boolQueryBuilder.filter(rangeQueryBuilder);
@@ -136,7 +140,7 @@ public class MallSearchServiceImpl implements MallSearchService {
          * 3.排序，分页，高亮
          */
         // 3.1排序
-        if(StringUtils.isNotBlank(param.getSort())){//sort=hotScore_acs/desc
+        if (StringUtils.isNotBlank(param.getSort())) {//sort=hotScore_acs/desc
             String[] str = param.getSort().split("_");
             SortOrder order = str[1].equalsIgnoreCase("asc") ? SortOrder.ASC : SortOrder.DESC;
             searchSourceBuilder.sort(str[0], order);
@@ -146,27 +150,46 @@ public class MallSearchServiceImpl implements MallSearchService {
         // pageNum 1 from 0 size 5
         // pageNum 2 from 5 size 5
         // pageNum n from:(n-1)*size
-        searchSourceBuilder.from((param.getPageNum() - 1)*EsConstant.PRODUCT_PAGESIZE);
+        searchSourceBuilder.from((param.getPageNum() - 1) * EsConstant.PRODUCT_PAGESIZE);
         searchSourceBuilder.size(EsConstant.PRODUCT_PAGESIZE);
 
         // 3.3高亮
-        if(StringUtils.isNotBlank(param.getKeyword())){
+        if (StringUtils.isNotBlank(param.getKeyword())) {
             HighlightBuilder highlightBuilder = new HighlightBuilder();
             highlightBuilder.field("skuTitle").preTags("<b sytel='color:red'>").postTags("</b>");
             searchSourceBuilder.highlighter(highlightBuilder);
         }
 
-        String s = searchSourceBuilder.toString();
-        System.out.println("构建的DSL:"+s);
 
         /**
-         * 聚合分析
+         * 4.聚合分析
          */
-        return new SearchRequest(new String[]{EsConstant.PRODUCT_INDEX},searchSourceBuilder);
+        // 4.1 品牌聚合
+        TermsAggregationBuilder brandAgg = AggregationBuilders.terms("brand_agg").field("brandId").size(50);
+        // 品牌聚合的子聚合
+        brandAgg.subAggregation(AggregationBuilders.terms("brand_name_agg").field("brandName").size(1));
+        brandAgg.subAggregation(AggregationBuilders.terms("brand_img_agg").field("brandImg").size(1));
+        // 4.2 分类聚合
+        TermsAggregationBuilder catelogAgg = AggregationBuilders.terms("catelog_agg").field("catalogId").size(50);
+        catelogAgg.subAggregation(AggregationBuilders.terms("catelog_name_agg").field("catalogName").size(1));
+        // 4.3 属性聚合
+        NestedAggregationBuilder attrsAgg = AggregationBuilders.nested("attr_agg", "attrs");
+        TermsAggregationBuilder attrIdAgg = AggregationBuilders.terms("attr_id_agg").field("attrs.attrId").size(50);
+        attrIdAgg.subAggregation(AggregationBuilders.terms("attr_name_agg").field("attrs.attrName").size(1));
+        attrIdAgg.subAggregation(AggregationBuilders.terms("attr_value_agg").field("attrs.attrValue").size(50));
+        attrsAgg.subAggregation(attrIdAgg);
+        // 将其加入到sourceBuilder
+        searchSourceBuilder.aggregation(brandAgg);
+        searchSourceBuilder.aggregation(catelogAgg);
+        searchSourceBuilder.aggregation(attrsAgg);
+        String s = searchSourceBuilder.toString();
+        System.out.println("构建的DSL:" + s);
+        return new SearchRequest(new String[]{EsConstant.PRODUCT_INDEX}, searchSourceBuilder);
     }
 
     /**
      * 把响应数据封装成我们需要的格式
+     *
      * @param searchResponse
      * @return
      */
