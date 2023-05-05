2db70f09ecce65db46ecb7eb7166f69fed59c95d
Leo <2422737092@qq.com>
2023-05-05 16:13:31 +0800

    认证服务-验证码倒计时

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/gulimall/config/GulimallWebConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.gulimall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author lxy
+ * @version 1.0
+ * @Description 视图配置器
+ * @date 2023/5/5 16:07
+ */
+@Configuration
+public class GulimallWebConfig implements WebMvcConfigurer {
+
+    /**
+     * 视图映射,配置路径和视图的对应关系
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/register.html").setViewName("register");
+    }
+}
--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/gulimall/controller/IndexController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/gulimall/controller/IndexController.java
@@ -9,17 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @Description
  * @date 2023/5/5 13:59
  */
-@Controller
-public class IndexController {
-    @GetMapping("/login.html")
-    public String loginPage(){
-        return "login";
-    }
-
-    @GetMapping("/register.html")
-    public String registerPage(){
-        return "register";
-    }
-
-
-}
+// @Controller
+// public class IndexController {
+//     @GetMapping("/login.html")
+//     public String loginPage(){
+//         return "login";
+//     }
+//
+//     @GetMapping("/register.html")
+//     public String registerPage(){
+//         return "register";
+//     }
+//
+//
+// }
